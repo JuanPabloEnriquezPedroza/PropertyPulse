ac4920398331d9126b0f302cb39a5d455d55dc66
unknown <0228903@up.edu.mx>
2024-08-02 12:51:38 -0600

    Added not found and loading pages

---
+++ app/loading.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const LoadingPage = () => {
+  return (
+    <ClipLoader
+      size={150}
+      color="#3B82f6"
+      cssOverride={override}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+
+export default LoadingPage;
